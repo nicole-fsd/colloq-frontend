46e7cc551fa6019e88c58ce86958bcdffad6dd39
nicole-fsd <nlf4@case.edu>
2020-05-31 22:36:56 +0200

    update

--- src/data/index.js
+++ src/data/index.js
@@ -5,13 +5,17 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import userReducer from "./auth";
 
+const persistConfig = {
+  key: 'user',
+  storage,
+}
 
-const persistedUser = persistReducer({ key: "user", storage }, userReducer);
+const persistedUser = persistReducer(persistConfig, userReducer);
 
 export const store = createStore(
   combineReducers({
     user: persistedUser,
-  }),
-  composeWithDevTools(applyMiddleware(thunk))
-);
+  }), composeWithDevTools(applyMiddleware(thunk)));
+
+  
 export const persistor = persistStore(store);
